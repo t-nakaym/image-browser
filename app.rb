ac7680095f25ebef9ed628141afea7e4982c4812
t-nakaym <tnakaym@gmail.com>
2013-02-09 17:00:30 +0900

    Add a feature listing directories in the specified directory

--- app.rb
+++ app.rb
@@ -1,5 +1,30 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/paginate'
 
-get '/' do
-  'Hello, world!'
+
+BASE_DIRECTORY = ARGV[0]
+
+Struct.new('Result', :total, :size, :entries)
+
+class App < Sinatra::Base
+  register Sinatra::Paginate
+
+  helpers do
+    def page
+      [params[:page].to_i - 1, 0].max
+    end
+  end
+
+  get '/' do
+    all_entries = Dir.entries(BASE_DIRECTORY).select do |entry|
+      entry !~ /\A\.{1,2}\z/ && File.directory?(File.join(BASE_DIRECTORY, entry))
+    end
+
+    @entries = all_entries[page * 10, 10]
+    @result = Struct::Result.new(all_entries.size, @entries.size, @entries)
+
+    haml :list, :format => :html5
+  end
+
+  run! if app_file == $0
 end
